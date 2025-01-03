2ae59c51aac1f386c383f2a6160424b28d016021
YvonneHe <xiaoheiheide@gmail.com>
2025-01-03 16:33:00 +0800

    feat: 创建question 模块

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 路由全局前缀
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3005);
 }
 bootstrap();
---
+++ src/question/question.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Patch, Query, Param, Body } from '@nestjs/common';
+import { QuestionDto } from './dto/question.dto';
+
+@Controller('question')
+export class QuestionController {
+  @Get()
+  findAll(
+    @Query('keyword') keyword: string,
+    @Query('page') page: number,
+    @Query('pageSize') pageSize: number,
+  ) {
+    console.log(keyword, page, pageSize);
+    return {
+      list: ['a', 'b'],
+      count: 13,
+    };
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return {
+      id,
+      title: 'sss',
+      desc: 'hhh',
+    };
+  }
+
+  @Patch(':id')
+  updateOne(@Param('id') id: string, @Body() questionDto: QuestionDto) {
+    console.log(...questionDto);
+    return {
+      id,
+      title: 'sss',
+      desc: 'hhh',
+    };
+  }
+}
