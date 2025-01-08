f1b4dc20cfb85ac55767fc4118f7a9f855f1c02d
YvonneHe <xiaoheiheide@gmail.com>
2025-01-08 11:38:06 +0800

    feat: 增加guard校验

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +19,11 @@ export class AuthController {
     const { username, password } = userInfo;
 
     return await this.AuthService.signIn(username, password);
+  }
+
+  @UseGuards(AuthGuard) // 放在路由前先解密，并将user信息放在了 request 中
+  @Get('profile')
+  async getProfile(@Request() req) {
+    return req.user;
   }
 }
