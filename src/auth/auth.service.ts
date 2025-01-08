e8c63f80af79554f92be2ecf92f7b1c290943395
YvonneHe <xiaoheiheide@gmail.com>
2025-01-08 11:23:13 +0800

    feat: 登录返回token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,17 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   // 依赖注入
-  constructor(private readonly UserService: UserService) {}
+  constructor(
+    private readonly UserService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   // 校验用户
-  async signIn(username: string, password: string) {
+  async signIn(username: string, password: string): Promise<{ token: string }> {
     const user = await this.UserService.findOne(username, password);
 
     if (!user) {
@@ -15,8 +19,10 @@ export class AuthService {
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { password: p, ...userInfo } = user.toObject();
+    const { password: p, ...userInfo } = user.toObject(); // 不返回 password 字段
 
-    return userInfo; // 不返回 password 字段
+    return {
+      token: await this.jwtService.signAsync(userInfo),
+    };
   }
 }
